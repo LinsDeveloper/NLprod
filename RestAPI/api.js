b892456cf5ea729c0500e1a49bc929745e452868
PEDRO LINS <pedrolins42@gmail.com>
2023-01-15 01:09:34 -0300

    Inicializacao da tela de login.

--- RestAPI/api.js
+++ RestAPI/api.js
@@ -1,14 +1,36 @@
-const WS = require('./WS');
+const WS = require('../../NLprod/');
+require("dotenv").config();
 
 var express = require('express');
+const session = require('express-session');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var port = process.env.PORT;
 
 
 
 
 var app = express();
 var router = express.Router();
+
+
+app.use(session({secret:'njafknqjdinfgswjgndjwngjfnwdjifd'}));
+
+
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "../login.html")
+})
+
+
+
+app.post('/login',(req, res)=>{
+    usuario = req.body.login;
+    console.log(usuario);
+})
+
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -24,6 +46,6 @@ router.route('/Usuarios').get((request, response) => {
 })
 
 
-var port = process.env.PORT || 8090;
+
 app.listen(port);
 console.log('WS iniciado na porta : ' + port)
