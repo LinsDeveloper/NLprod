93e1445b5a6b68a4f2d5d2d2d01ab2fdb260b2f2
Thainá de Sá <thaina.sa@sislogica.com.br>
2023-03-23 17:12:06 -0300

    dsa

--- styles/ProdJs/Call.js
+++ styles/ProdJs/Call.js
@@ -4,25 +4,7 @@ function loginRequest(token){
    
 
    
-    var obj={DstokenOne: `${token}`};
-    var toUrl = "/AtualizaAutenticacao";
-    var method="POST";
-
-    
-    WebService.Init(obj, toUrl, CallBack, method);
-
-
-
-
-    function CallBack(response){
-
-
-        console.log(response);
-        
-
-
-        
-    }
+ 
 
 
 }
